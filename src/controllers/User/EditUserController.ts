23d2101f638de4b6d00ac35546fbfbeed266c67d
José Bonifácio <boninho7834@gmai.com>
2025-04-22 02:35:37 -0300

    ajust password

--- src/controllers/User/EditUserController.ts
+++ src/controllers/User/EditUserController.ts
@@ -1,33 +1,40 @@
-import { Request, Response } from 'express';
-import { EditUserService } from '../../services/User/EditUserService';
+import { Request, Response } from "express";
+import { EditUserService } from "../../services/User/EditUserService";
 
 class EditUserController {
-    async handle(req: Request, res: Response) {
+  async handle(req: Request, res: Response) {
+    const { password, name, email, role, oab, observation } = req.body;
 
-        const { password, passwordOld, name, email, role, oab, observation } = req.body
+    let { userId } = req.params;
 
-        let { userId } = req.params
+    let photo = "";
 
-        let photo = ""
-
-        if (req.file) {
-            photo = req.file.filename
-        }
-        
-        let userIdAuth = req.userId
-
-        const editUserService = new EditUserService
-
-        const user = await editUserService.execute({
-            password, passwordOld, name, email, role, oab, photo, observation, userId: userId || userIdAuth
-        })
-
-        if (user["photo"]) {
-            user["photo_url"] = "https://dangersjorge-data.s3.sa-east-1.amazonaws.com/" + user["photo"];
-        }
-        
-        return res.json(user)
+    if (req.file) {
+      photo = req.file.filename;
     }
+
+    let userIdAuth = req.userId;
+
+    const editUserService = new EditUserService();
+
+    const user = await editUserService.execute({
+      password,
+      name,
+      email,
+      role,
+      oab,
+      photo,
+      observation,
+      userId: userId || userIdAuth,
+    });
+
+    if (user["photo"]) {
+      user["photo_url"] =
+        "https://dangersjorge-data.s3.sa-east-1.amazonaws.com/" + user["photo"];
+    }
+
+    return res.json(user);
+  }
 }
 
-export { EditUserController }
+export { EditUserController };
